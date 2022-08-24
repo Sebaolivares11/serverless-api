f9592e02b4daefe201fd953c4cb3e55a5350835f
Seba Olivares <sebastian.olivaresp@utem.cl>
2022-08-24 16:37:16 -0400

    add new methods and endpoints

--- handler.py
+++ handler.py
@@ -2,6 +2,7 @@ import json
 import boto3
 import logging
 import os
+import uuid
 
 from custom_encoder import CustomEncoder
 logger = logging.getLogger()
@@ -12,24 +13,25 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(dynamodbTableName)
 
 getMethod = 'GET'
-postmethod = 'POST'
+postMethod = 'POST'
 patchMethod = 'PATCH'
 deleteMethod = 'DELETE'
 
 usersPath = '/users'
 
 
-def post_users(event, context):
+def manage_request(event, context):
     logger.info(event['body'])
     httpmethod = event['httpMethod']
     path = event['path']
     if httpmethod == getMethod and path == usersPath:
         response = buildResponse(200)
-    if httpmethod == postmethod and path == usersPath:
-        response = saveUser(json.loads(event['body']))
-    #elif httpmethod == patchMethod and path == usersPath:
-    #    requestBody = json.loads(event['body'])
-    #    response = modifyUser(requestBody['id'], requestBody['updateKey'], requestBody['updateValue'])
+    elif httpmethod == postMethod and path == usersPath:
+        requestBody = json.loads(event['body'])
+        response = saveUser(requestBody['username'])
+    elif httpmethod == patchMethod and path == usersPath:
+        requestBody = json.loads(event['body'])
+        response = modifyUser(requestBody['id'], requestBody['updateKey'], requestBody['updateValue'])
     else:
         response = buildResponse(404, 'Not Found')
     return response
@@ -37,24 +39,29 @@ def post_users(event, context):
 
 def saveUser(requestBody):
     try:
-        table.put_item(Item=requestBody)
+        key = uuid.uuid4().hex
+        table.put_item(
+            Item={
+                'id': key,
+                'username': requestBody
+                })
         body= {
             'Operation': 'SAVE',
             'Message': 'SUCCESS',
-            'item': requestBody
+            'item': { 'id': key, 'username': requestBody }
         }
         return buildResponse(200, body)
     except:
         logger.exception('Error creating user')
 
 
-def modifyUser(userId, updateKey,updateValue):
+def modifyUser(userId, updateKey, updateValue):
     try:
         response = table.update_item(
-            key={ 'id': userId },
+            Key={ 'id': userId },
             UpdateExpression = 'set %s = :value '% updateKey,
             ExpressionAttributeValues={ ':value': updateValue },
-            returnValues= 'UPDATED_NEW'
+            ReturnValues= 'UPDATED_NEW'
         )
         body={
             'Operation': 'UPDATE',
